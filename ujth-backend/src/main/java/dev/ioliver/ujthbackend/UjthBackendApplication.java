6aa64e16656c9f8211cbb14748948445aa2d09f6
Igor Oliveira <i.oliveira@email.com>
2023-05-29 22:42:30 -0300

    base commit

---
+++ ujth-backend/src/main/java/dev/ioliver/ujthbackend/UjthBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.ioliver.ujthbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UjthBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UjthBackendApplication.class, args);
+  }
+
+}
---
+++ ujth-backend/src/test/java/dev/ioliver/ujthbackend/UjthBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ioliver.ujthbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UjthBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
